d1a768c296f462343a0f6bf5dd1c6aa379c52f89
Gracerue <munyaradzi045@gmail.com>
2023-03-15 09:41:19 +0200

    Add

--- src/index.js
+++ src/index.js
@@ -1,48 +1,7 @@
 import "./style.css";
-import leaderboard from "./modules/leaderboard.js";
+
 import UI from "./modules/display.js"
 
-const name = document.querySelector('#name');
-const yourScore = document.querySelector('#your-score');
-const submitBtn = document.querySelector('#submit');
-const resetBtn = document.querySelector('#results-btn');
 
-const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/BbPqSVwFE639mX4LkLcu'
-const board = new leaderboard(url);
-const apiRequest = `${url}/scores/`;
-board.getScore(apiRequest);
-
-submitBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    if (!name.value.trim()) {
-      name.setCustomValidity('Type in your username.');
-      name.reportValidity();
-    } else if (!yourScore.value) {
-      yourScore.setCustomValidity('Fill in your score.');
-      yourScore.reportValidity();
-    } else {
-      board.addValues(apiRequest, name.value.trim(), yourScore.value);
-      submitBtn.setCustomValidity('"Leaderboard score created correctly.');
-      submitBtn.reportValidity();
-      name.value = '';
-      yourScore.value = '';
-    }
-    setTimeout(() => {
-      board.getScore(apiRequest);
-      JSON.parse(localStorage.getItem('Scores'));
-    }, 1000);
-  });
-
-  resetBtn.addEventListener('click', () => {
-    board.getScore(apiRequest);
-    setTimeout(() => {
-      if (localStorage.getItem('Score')) {
-        const results = JSON.parse(localStorage.getItem('Score'));
-        const scoreArray = results.result;
-        UI.displayScore(scoreArray);
-      }
-    }, 1000);
-    location.reload();
-  });
 
 document.addEventListener('DOMContentLoaded', UI.displayScore);

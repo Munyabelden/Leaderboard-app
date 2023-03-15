bfcf996cfb0a7b89142ca3a0f385740663e7a46c
Gracerue <munyaradzi045@gmail.com>
2023-03-15 09:58:42 +0200

    Add reset and submit functionality

---
+++ src/modules/game.js
@@ -0,0 +1,40 @@
+export default class Game {
+    constructor(url) {
+        this.url = url
+    }
+
+     game = async (url) => {
+        await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ name: 'Belden`s Game' }),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            localStorage.setItem('gameID', JSON.stringify(data));
+          });
+      };
+
+     getScore = async (url) => {
+        await fetch(url, {
+          method: 'GET',
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            localStorage.setItem('Scores', JSON.stringify(data));
+          });
+      };
+
+      addValues = async (url, name, score) => {
+        await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+          body: JSON.stringify({ user: name, score }),
+        })
+          .then((response) => response.json());
+      };
+};
